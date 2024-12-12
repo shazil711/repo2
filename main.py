fd85371a9a759183fe4cb8dccbaea8e127016e25
shazil711 <shazi85@gmail.com>
2024-12-12 19:01:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,16 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return 'Welcome to Python Flask World V1.0 from CI/CD pipeline'
+import random
 
+
+
+result = random.randint(0,1)
+
+if result == 0:
+    print("tails")
+else:
+    print("Heads")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
