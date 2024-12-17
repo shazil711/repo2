4b2e89b2d1ad7763434d47f277edae9b735108cd
shazil711 <shazi85@gmail.com>
2024-12-17 14:08:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,20 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def area_of_square(side):
-    area = int(side * side)
-    print(area)
- 
+student_scores = [80, 60, 50, 65, 75, 55]
 
 
-area_of_square(6)
+def sum_score(student_scores):
+    sum =  0
+    no_students = 0
 
+    for score in student_scores:
+        sum += score
+        no_students = no_students + 1
+
+    average_score = sum / no_students
+    return average_score
+
+print(sum_score(student_scores))
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
